88d24860cafb6ce32a6a85b82b9165d767fe3dd8
조재민 <JJM3485>
2026-01-12 21:14:26 +0900

    First Commit: Project Init & DB Setup

---
+++ src/main/java/com/example/chatping/ChatpingApplication.java
@@ -0,0 +1,13 @@
+package com.example.chatping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatpingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatpingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/chatping/ChatpingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chatping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatpingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
